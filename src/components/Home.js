2ac7d74535e15a4ed205065b66e24dada686094b
Mahmoud-IG <mahmoud.16.6.ig@gmail.com>
2024-07-04 03:11:50 +0300

    mute the video

--- src/components/Home.js
+++ src/components/Home.js
@@ -39,7 +39,7 @@ const Home = () => {
   return (
     <Container>
       <VideoWrapper>
-        <StyledVideo ref={videoRef} loop autoPlay>
+        <StyledVideo ref={videoRef} loop autoPlay muted>
           <source src={videoSrc} type="video/mp4" />
           Your browser does not support the video tag.
         </StyledVideo>
